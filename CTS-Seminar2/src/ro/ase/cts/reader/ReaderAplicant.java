ca3173eb189b1eed62f1d36c9764f2aa6e78cfb2
AlexandraPupaza <pupazaalexandra18@stud.ase.ro>
2021-03-26 15:07:28 +0200

    Primul commit

---
+++ CTS-Seminar2/src/ro/ase/cts/reader/ReaderAplicant.java
@@ -0,0 +1,19 @@
+package ro.ase.cts.reader;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+import ro.ase.cts.clase.Aplicant;
+
+public abstract class ReaderAplicant {
+	private String numeFisier;
+	public abstract List<Aplicant> readAplicanti() throws FileNotFoundException;
+	public ReaderAplicant(String numeFisier) {
+		super();
+		this.numeFisier = numeFisier;
+	}
+	
+	
+	
+
+}
