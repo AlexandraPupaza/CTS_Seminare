6547a23833ec5dbbdb89c9cae3ebbd02c073163e
AlexandraPupaza <pupazaalexandra18@stud.ase.ro>
2021-04-20 13:56:43 +0300

    Problema9-ProxyRezervare

---
+++ Seminar9/src/ro/ase/cts/proxy/clase/ProxyRezervare.java
@@ -0,0 +1,27 @@
+package ro.ase.cts.proxy.clase;
+
+public class ProxyRezervare implements IOperatorRezervari  {
+	
+	private OperatorRezervare operatorRezervare;
+	private int nrMinimDePersoane;
+	
+
+	public ProxyRezervare(OperatorRezervare operatorRezervare, int nrMinimDePersoane) {
+		super();
+		this.operatorRezervare = operatorRezervare;
+		this.nrMinimDePersoane = nrMinimDePersoane;
+	}
+
+
+	@Override
+	public void rezerva(int nrPersoane) {
+	if(nrPersoane >= nrMinimDePersoane) {
+		operatorRezervare.rezerva( nrPersoane );
+	}
+	else {
+		System.out.println("Numarul de persoane este prea mic!");
+	}
+		
+	}
+
+}
