0bf625f1c085c551f6fee755f824f52d9383dccf
AlexandraPupaza <pupazaalexandra18@stud.ase.ro>
2021-05-22 10:23:52 +0300

    Seminar12-JUnit-StudentTests-Constructori si metode

---
+++ Seminar12/src/tests/StudentTests.java
@@ -0,0 +1,39 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import clase.Student;
+
+public class StudentTests {
+
+	@Test
+	public void testCorectitudineConstructorCuParametru() {
+		String nume = "Gigel";
+		Student student = new Student(nume);
+		assertEquals(nume,student.getNume());
+	}
+	
+	@Test
+	public void testInitializareListaInConstructorCuParametru() {
+		Student student = new Student("Gigel");
+		//assertNotEquals(null,student.getNote());
+		assertNotNull(student.getNote());
+	}
+	@Test
+	public void testCorectitudineConstructorDefault() {
+		Student student = new Student();
+		assertNotNull("Numele nu a fost initializat", student.getNume());
+		assertNotNull("Lista nu a fost initializata", student.getNote());
+	}
+	
+	@Test
+	public void testMetodaAdaugareNotaInLista() {
+		Student student = new Student();
+		int nota =10;
+		student.adaugaNota(nota);
+		assertEquals(nota,student.getNota(0));
+	}
+
+}
